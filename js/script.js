88496ee51061d13ceae43a3a179eb78f675b9b13
Alfonso <alfof87@gmail.com>
2020-07-28 10:59:08 +0200

    ajax-ex-boolfix errore

--- js/script.js
+++ js/script.js
@@ -1,40 +1,73 @@
-function functionName() {
+function addSearchClickListener(){
+  var target = $("#search");
+  target.click(getMovies);
+}
+function getMovies() {
+  var target = $("#query");
+  var query = target.val();
+
   $.ajax({
-    url: "https://api.themoviedb.org/3/search/movie",
-    method: "GET",
-    data: {
-			api_key: "d7a215969de8ee9ea8bd1af46e9cf6f0",
-      query: "title"
+
+        url: "https://api.themoviedb.org/3/search/movie",
+        method: "GET",
+        data: {
+    			api_key: "d7a215969de8ee9ea8bd1af46e9cf6f0",
+          query: query
 
     },
     success: function(data){
+      var movies = data["results"];
+      var target = $("#results ul");
+      var template = $("#movie-template").html();
+      var compiled = Handlebars.compile(template);
 
-      var title = data["result"]["title"];
-      var originalT = data["result"]["original_title"];
-      var originalL = data["result"]["original_language"];
-      var vote = data["result"]["vote_average"];
-      for (var i = 0; i < result.length; i++) {
-        var element = $(".film");
-        if (result[i] == 1) {
-          element.append(result[i]);
-        }
+      for (var i = 0; i < movies.length; i++) {
+        var movie = movies[i];
+        var movieHTML = compiled(movie);
+        target.append(movieHTML);
       }
     },
-    error: function(error){
-      console.log("error", error);
+    error: function (err){
+      console.log(err);
     }
-
-  });
+  })
 }
+// function functionName() {
+//   $.ajax({
+//     url: "https://api.themoviedb.org/3/search/movie",
+//     method: "GET",
+//     data: {
+// 			api_key: "d7a215969de8ee9ea8bd1af46e9cf6f0",
+//       query: "title"
+//
+//     },
+//     success: function(data){
+//
+//       var title = data["result"]["title"];
+//       var originalT = data["result"]["original_title"];
+//       var originalL = data["result"]["original_language"];
+//       var vote = data["result"]["vote_average"];
+//       for (var i = 0; i < result.length; i++) {
+//         var element = $(".film");
+//         if (result[i] == 1) {
+//           element.append(result[i]);
+//         }
+//       }
+//     },
+//     error: function(error){
+//       console.log("error", error);
+//     }
+//
+//   });
+// }
 
-// Di conseguenza quando andata a fare la success dell'ajax avete un data
-// che contiene solo le seguenti chiavi: page, total_reulsts, total_pages,
-// results
+
 
 
 
 
 function init(){
-  functionName();
+  addSearchClickListener();
+
 }
 $(document).ready(init);
